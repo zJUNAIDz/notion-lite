1020e7fd99d0bd0ff92715e2988bb4afe06d8741
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-11-22 20:14:55 +0530

    Set up images and logos organized initial folders and routes

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+  return (
+    <div className="min-h-full flex flex-col ">
+      <div className="flex flex-col items-center justify-center md:justify-start gap=y-8  flex-1 px-1"></div>
+    </div>
+  );
+};
+
+export default TestPage;
