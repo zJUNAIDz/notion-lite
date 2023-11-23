df3d721b12a01aa1265502b9974bb1624eab7e3d
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-11-23 19:21:09 +0530

    Add: Footer Logo and Buttons

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
 import Logo from "./Logo";
 
 const Footer = () => {
   return (
     <div className="flex items-center w-full p-6 bg-background z-2">
       <Logo />
+      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms &amp; Conditions
+        </Button>
+      </div>
     </div>
   );
 };
