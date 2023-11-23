9137973c7c16efef0a57c13d4c464655b29d33f0
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-11-23 19:10:41 +0530

    add Footer Logo

--- app/(marketing)/_components/Logo.tsx
+++ app/(marketing)/_components/Logo.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+const fonts = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
 const Logo = () => {
-  return <div></div>;
+  return (
+    <div className=" hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" alt="Logo" height="40" width="40" />
+      <p className={cn("font-semibold", fonts.className)}>Notion</p>
+    </div>
+  );
 };
 
 export default Logo;
