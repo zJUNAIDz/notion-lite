a01ac422b288faec284388e2dc10440d57ade128
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-11-24 20:12:30 +0530

    Add SignIn and SignUp buttons in Navbar

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -2,10 +2,15 @@
 import { useScrollTop } from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
+
 import { ModeToggle } from "@/components/mode-toggle";
-import { SignIn } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, useUser } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+  const { user } = useUser();
   const isScrolled = useScrollTop();
 
   return (
@@ -17,7 +22,21 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full items-center flex gap-x-2">
-        <ModeToggle /> 
+        {isLoading && <p>Loading...</p>}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">Get Notion Free</Button>
+            </SignUpButton>
+          </>
+        )}
+        {isAuthenticated && <p>Logged In as {user?.username}</p>}
+        <ModeToggle />
       </div>
     </div>
   );
