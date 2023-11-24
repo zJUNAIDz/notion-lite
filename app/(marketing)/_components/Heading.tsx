7b1826a4f548ff2df88789ebecf68509b5b00a15
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-11-24 22:40:28 +0530

    Update Auth button in Header component

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,8 +1,13 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import Spinner from "@/components/ui/spinner";
 import { SignUpButton } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-xl h-full space-y-4">
       <h1 className=" text-3xl sm:text-5xl md:text-5xl font-bold">
@@ -13,11 +18,23 @@ const Heading = () => {
         Notion is the conected Workspace where, <br />
         Better, faster Work happens.
       </h3>
-      <SignUpButton mode="modal">
-        <Button className=" ">
-          Get Started <ArrowRight className=" h-4 w-4 ml-2" />
+      {isLoading && (
+        <div className="flex justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {!isLoading && !isAuthenticated && (
+        <SignUpButton mode="modal">
+          <Button>
+            Get Started <ArrowRight className=" h-4 w-4 ml-2" />
+          </Button>
+        </SignUpButton>
+      )}
+      {!isLoading && isAuthenticated && (
+        <Button>
+          <Link href="/documents">Enter Notion</Link>
         </Button>
-      </SignUpButton>
+      )}
     </div>
   );
 };
