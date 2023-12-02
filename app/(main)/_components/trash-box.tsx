1a198c39cc801cc7852aa642d1c26b7de18639a8
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-02 12:04:06 +0530

    Add - trash box component

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { Input } from "@/components/ui/input";
 import Spinner from "@/components/ui/spinner";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import { Search, Trash, Undo } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
@@ -38,6 +40,7 @@ const TrashBox = () => {
   };
 
   const onDelete = (documentId: Id<"documents">) => {
+  
     const promise = deleteNote({ id: documentId });
     toast.promise(promise, {
       loading: "Deleting note...",
@@ -50,16 +53,59 @@ const TrashBox = () => {
     }
   };
 
+
   if (documents === undefined)
     return (
       <>
-        <p className=" text-center">Trash Box</p>
         <div className="h-[5rem] flex items-center justify-center">
           <Spinner size="default" />
         </div>
       </>
     );
-  return <p>Trash box</p>;
+  return (
+    <div className=" text-sm">
+      <div className=" flex items-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className=" h-7 px-2 focus-visible:ring-transparent bg-secondary"
+          placeholder="Filter by page titile"
+        />
+      </div>
+      <div className="  mt-2 px-1 pb-1">
+        <p className="hidden last:block text-center text-muted-foreground pb-2">
+          No Documents found!
+        </p>
+        {filteredDocument?.map((document) => (
+          <div
+            key={document._id}
+            role="button"
+            onClick={() => onClick(document._id)}
+            className=" text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between "
+          >
+            <span className="truncate pl-2">{document.title}</span>
+            <div className=" flex items-center">
+              <div
+                role="button"
+                onClick={(e) => onRestore(e, document._id)}
+                className=" rounded-sm p-2 hover:bg-neutral-200 "
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div
+                role="button"
+                onClick={() => onDelete(document._id)}
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Trash className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </div>
+          </div>
+        ))} 
+      </div>
+    </div>
+  );
 };
 
 export default TrashBox;
