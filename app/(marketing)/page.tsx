f4770a28031784f3bad82889b55fa22c04d4ed53
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-08 01:06:06 +0530

    fix:(style) remove extra gap in small devices

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,9 @@ import Heroes from "./_components/Heroes";
 const TestPage = () => {
   return (
     <div className="min-h-full flex flex-col ">
-      <div className="flex flex-col items-center justify-center md:justify-start gap=y-8  flex-1 px-1 pb-10 text-center">
+      <div
+        className="flex flex-col items-center justify-center md:justify-start md:gap-y-8 flex-1 px-1 pb-10 text-center"
+      >
         <Heading />
         <Heroes />
       </div>
