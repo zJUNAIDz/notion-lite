0b1afcf446d10423735c60d19d64646485370cf2
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-08 01:58:40 +0530

    feat: (style) add Emoji icon selector

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -6,7 +6,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Button } from "./ui/button";
 import { ElementRef, useRef, useState } from "react";
-
+import TextAreaAutosize from "react-textarea-autosize";
 interface Props {
   initialData: Doc<"documents">;
   preview?: boolean;
@@ -15,12 +15,12 @@ interface Props {
 const Toolbar = ({ initialData, preview }: Props) => {
   const changeIcon = useMutation(api.documents.update);
 
-  const inputRef = useRef<ElementRef<"textarea">>();
+  const inputRef = useRef<ElementRef<"textarea">>(null);
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState(initialData.title);
 
   const update = useMutation(api.documents.update);
-
+  const removeIcon = useMutation(api.documents.removeIcon);
   const enableInput = () => {
     //* Just in case a heckar manages to trigger this function 😅
     if (preview) return;
@@ -39,17 +39,25 @@ const Toolbar = ({ initialData, preview }: Props) => {
     update({ id: initialData._id, title: value || "Untitled" });
   };
 
-  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
+  const onKeyDown = (ev: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (ev.key === "Enter") {
+      ev.preventDefault();
       disableInput();
     }
   };
 
+  const onIconSelect = (icon: string) => {
+    update({ id: initialData._id, icon });
+  };
+
+  const onRemoveIcon = () => {
+    removeIcon({ id: initialData._id });
+  };
+
   return (
-    <div className=" pl-[40px]">
+    <div className=" pl-[40px] group/icon">
       {!!initialData.icon && !preview && (
-        <div className=" flex items-center gap-x-2 group/icon pt-6">
+        <div className=" flex items-center gap-x-2  pt-6">
           <IconPicker
             onChange={(emoji) => {
               changeIcon({ id: initialData._id, icon: emoji });
@@ -62,6 +70,7 @@ const Toolbar = ({ initialData, preview }: Props) => {
           <Button
             className=" bg-transparent rounded-full opacity-0 group-hover/icon:opacity-75 transition text-muted-foreground text-xs"
             variant="outline"
+            onClick={onRemoveIcon}
           >
             <X className="h-4 w-4 " size="icon" />
           </Button>
@@ -70,9 +79,9 @@ const Toolbar = ({ initialData, preview }: Props) => {
       {!!initialData.icon && preview && (
         <p className="text-6xl pt-6">{initialData.icon}</p>
       )}
-      <div className="md:opacity-100 group-hover/icon:opacity-75 flex items-center gap-x-1 py-4">
+      <div className="opacity-100 md:opacity-0 group-hover/icon:opacity-100 flex items-center gap-x-1 py-4">
         {!initialData.icon && !preview && (
-          <IconPicker onChange={() => {}} asChild>
+          <IconPicker onChange={onIconSelect} asChild>
             <Button
               className=" bg-transparent text-muted-foreground text-xs"
               variant="outline"
@@ -95,6 +104,23 @@ const Toolbar = ({ initialData, preview }: Props) => {
           </Button>
         )}
       </div>
+      {isEditing && !preview ? (
+        <TextAreaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={title}
+          onChange={(e) => onInput(e.target.value)}
+          className=" text-5xl bg-transparent outline-none font-bold break-words text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11px] text-5xl font-bold break-words outline-none  text-[#3F3F3F] dark:text-[#CFCFCF]"
+        >
+          {initialData.title}
+        </div>
+      )}
     </div>
   );
 };
