c71c78f5d6a806980b53aa6accf65e72469209cf
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-09 21:34:21 +0530

    Style: add provider for all modals at one place

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { SettingsModal } from "../modals/settings-modal";
+import { CoverModal } from "../modals/cover-image-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <SettingsModal />
+      <CoverModal />
+    </>
+  );
+};
