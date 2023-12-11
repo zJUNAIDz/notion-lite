f75c61ba2c738336912dd17e44b05394e898d13a
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-11 21:45:43 +0530

    Style: add cover component for cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Cover } from "@/components/cover";
 import Toolbar from "@/components/toolbar";
 import Spinner from "@/components/ui/spinner";
 import { api } from "@/convex/_generated/api";
@@ -23,7 +24,7 @@ const DocumentIdPage = ({ params: { documentId } }: Props) => {
 
   return (
     <div className="pb-40 ">
-      <div className="h-[30vh]" />
+      <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-5xl mx-auto">
         <Toolbar initialData={document} preview={false} />
       </div>
