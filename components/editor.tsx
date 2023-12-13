4e5ca8e1355997350c1e574a7be861e488df47a5
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-13 19:37:27 +0530

    Fix: remove PartialBlock[] type

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteEditor, InlineContent, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 import { useTheme } from "next-themes";
@@ -8,20 +8,25 @@ interface Props {
   initialContent: string | undefined;
   editable?: boolean;
 }
+// type PartialBlock = {
+//   id?: string;
+//   type?: string;
+//   props?: Partial<Record<string, string>>;
+//   content?: string | InlineContent[];
+//   children?: BlockSpec[];
+// };
 
 export const Editor = ({ onChange, initialContent, editable }: Props) => {
   const { resolvedTheme } = useTheme();
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
-    initialContent: initialContent
-      ? (JSON.parse(initialContent) as PartialBlock[])
-      : undefined,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
     onEditorContentChange: (editor) => {
+      console.log(JSON.stringify(editor.topLevelBlocks));
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
   });
-
   return (
     <BlockNoteView
       editor={editor}
