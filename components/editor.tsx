51d974d9696265cdab59bdc3f828e60c0453605a
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-13 19:49:11 +0530

    Feat: upload image inside text editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,6 +3,7 @@ import { BlockNoteEditor, InlineContent, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 import { useTheme } from "next-themes";
+import { useEdgeStore } from "@/lib/edgestore";
 interface Props {
   onChange: (value: string) => void;
   initialContent: string | undefined;
@@ -18,7 +19,15 @@ interface Props {
 
 export const Editor = ({ onChange, initialContent, editable }: Props) => {
   const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
 
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+    return response.url;
+  };
+  
   const editor: BlockNoteEditor = useBlockNote({
     editable,
     initialContent: initialContent ? JSON.parse(initialContent) : undefined,
@@ -26,7 +35,9 @@ export const Editor = ({ onChange, initialContent, editable }: Props) => {
       console.log(JSON.stringify(editor.topLevelBlocks));
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
+    uploadFile: handleUpload,
   });
+
   return (
     <BlockNoteView
       editor={editor}
