0143d555ffbc113b9bd6e2c40cc2a55d8063791f
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-15 19:12:52 +0530

    Style: add popover to publish & unpublish document

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Popover, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { api } from "@/convex/_generated/api";
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import { useOrigin } from "@/hooks/use-origin";
 import { useMutation } from "convex/react";
+import { Check, Copy, Globe } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
@@ -58,15 +63,63 @@ const Publish = ({ initialData }: Props) => {
   return (
     <Popover>
       <PopoverTrigger>
-        <Button
-          className=" bg-transparent"
-          onClick={onPublish}
-          variant="outline"
-          size="sm"
-        >
+        <Button className=" bg-transparent" variant="outline" size="sm">
           Publish
         </Button>
       </PopoverTrigger>
+      <PopoverContent className=" w-72" align="end" alignOffset={8} forceMount>
+        {initialData.isPublished ? (
+          <div className="space-y-4">
+            <div className="flex items-center gap-x-2 text-blue-600 text-sm">
+              <Globe className="h-4 w-4 animate-pulse" /> The note is live on
+              web
+            </div>
+            <div className="flex gap-x-1 rounded-md overflow-hidden border">
+              <input
+                type="text"
+                value={url}
+                disabled
+                className="p-1 truncate"
+              />
+              <Button
+                onClick={onCopy}
+                disabled={copied}
+                className=" rounded-l-none"
+                size="sm"
+              >
+                {copied ? (
+                  <Check className="h-4 w-4" />
+                ) : (
+                  <Copy className="h-4 w-4" />
+                )}
+              </Button>
+            </div>
+            <Button
+              onClick={onUnPublish}
+              disabled={isSubmitting}
+              className="w-full"
+            >
+              Unpublish
+            </Button>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-y-3">
+            <Globe className="h-6 w-6" />
+            <p className="text-sm font-medium">Publish this note</p>
+            <span className="text-xs text-muted-foreground">
+              Share your work with others
+            </span>
+            <Button
+              disabled={isSubmitting}
+              onClick={onPublish}
+              size="sm"
+              className="w-full"
+            >
+              Publish
+            </Button>
+          </div>
+        )}
+      </PopoverContent>
     </Popover>
   );
 };
