eba87dd098aa7b586b2fbc41fd21ef92dea17428
Junaid Shaikh <64751110+zJUNAIDz@users.noreply.github.com>
2023-12-26 00:01:26 +0530

    Fix : display username if no name available

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -30,7 +30,7 @@ export const UserItem = () => {
               <AvatarImage src={user?.imageUrl} alt="User Avatar" />
             </Avatar>
             <span className="text-start font-medium line-clamp-1">
-              {user?.fullName}
+              {user?.fullName || user?.username}
             </span>
           </div>
           <ChevronsUpDown className="ml-2 h-4 w-4" />
@@ -39,14 +39,14 @@ export const UserItem = () => {
       <DropdownMenuContent>
         <div className="w-full p-2 flex flex-col gap-y-2 mr-4 ">
           <div className=" text-xs text-muted-foreground">
-            {user?.emailAddresses[0]?.emailAddress || "no mail"}
+            {user?.emailAddresses[0]?.emailAddress || "email not available"}
           </div>
           <div className=" flex gap-x-2 items-center">
             <Avatar className=" h-10 w-10">
               <AvatarImage src={user?.imageUrl} alt="User Avatar" />
             </Avatar>
             <span className=" font-medium ">
-              {user?.fullName}&apos;s Notion
+              {user?.fullName || user?.username}&apos;s Notion
             </span>
           </div>
           <Button className=" flex gap-x-2" onClick={signOut} variant="ghost">
